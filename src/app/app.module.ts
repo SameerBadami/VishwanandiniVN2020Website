8dc0e61642eb20ffd4e64f368da07f686a05aec4
Sameer Badami <sameer.badami@gmail.com>
2022-10-21 10:39:06 +0530

    Website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
@@ -12,7 +13,11 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public displayLanguage = 'KANNADA';
+  public topicsData: any;
+  public topicIconImagePathUrl: any ;
+
+  constructor(
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
+     this.getImageIconPath();
+  }
+
+  getImageIconPath(){
+    this.http.get('http://3.109.163.108:3000/api/topicsImageViewIconPath').subscribe( (resp: any) =>{
+      this.topicIconImagePathUrl = resp;
+      if(this.topicIconImagePathUrl == '' || this.topicIconImagePathUrl == null || this.topicIconImagePathUrl == undefined){
+        this.getImageIconPath();
+      } else {
+        this.getAllBlogs();
+      }
+    });
+  }
+
+  getAllBlogs(){
+    let a = this.topicIconImagePathUrl.path;
+    this.http.get('http://3.109.163.108:3000/api/getAllPublishedTopicsAPI').subscribe( (resp: any) =>{
+      resp.forEach(function (value) {
+            value.topicIconImagePath = a+'/'+value.topicIconImageName;
+       });
+       this.topicsData = resp;
+      console.log("HERERERER---"+JSON.stringify(resp));
+    });
+  }
+
+  changeToKannada(){
+    this.displayLanguage = 'KANNADA';
+  }
+
+  changeToEnglish(){
+    this.displayLanguage = 'ENGLISH';
+  }
+
+  changeToTelugu(){
+    this.displayLanguage = 'TELUGU';
+  }
+
+  changeToTamil(){
+    this.displayLanguage = 'TAMIL';
+  }
+
+  changeToHindi(){
+    this.displayLanguage = 'HINDI';
   }
 
 }
